3367b699001d89fb4033bfcc35ca6b62ef003226
TaylorDavis16 <sheldonlee016@gmail.com>
2023-12-01 00:27:19 -0500

    URL change

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ import AssignmentRoutes from "./assignments/routes.js";
 import cors from "cors";
 import mongoose from "mongoose";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
-mongoose.connect(CONNECTION_STRING);
+// mongoose.connect(CONNECTION_STRING);
+mongoose.connect('mongodb+srv://sheldonlee:sheldonlee@project-users.totspm0.mongodb.net/Kanbas?retryWrites=true&w=majority');
 
 import UserRoutes from "./users/routes.js";
 import "dotenv/config";
@@ -17,7 +18,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL,
+    origin: 'https://xingdong.netlify.app',
   })
 );
 const sessionOptions = {
